7aeeff50aee6118f28994700db6258c1e0904203
Dmitriy Borisov <dimaborus94@gmail.com>
2023-05-29 23:05:59 +0400

    showing errors with controllers

---
+++ src/App.tsx
@@ -0,0 +1,76 @@
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import './App.css'
+
+interface IFormInputs {
+  TextField: string
+  NumberField: number | string
+}
+
+export default function App() {
+  const { handleSubmit, control, reset, formState } = useForm<IFormInputs>({
+    defaultValues: {
+      TextField: '',
+      NumberField: ''
+    },
+    mode: 'all'
+  });
+  const onSubmit: SubmitHandler<IFormInputs> = data => {
+    console.log('data')
+    console.log(data)
+    console.log('formState')
+    console.log(formState)
+  };
+  // надо прописать функцию обработки сабмита с ошибками, тогда их можно будет выцепить
+  const onError = (errors: any, e: any) => {
+    console.log('formState')
+    console.log(formState) // а вот тут они так и не показываются
+    console.log('errors')
+    console.log(errors)
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit(onSubmit, onError)}>
+      <label>Text input</label>
+      <Controller
+        name="TextField"
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field, fieldState }) => <>
+          <input
+            value={field.value}
+            onChange={(e) => {
+              console.log('FieldState - TextField: ')
+              console.log(fieldState)
+              field.onChange(e)
+            }}
+          />
+          <span className="err">{!!fieldState.error && `error: ${fieldState.error.message}`}</span>
+        </>
+        }
+      />
+      <label>Number input</label>
+      <Controller
+        name="NumberField"
+        control={control}
+        rules={{
+          required: true,
+          pattern: /\d{4}$/,
+        }}
+        render={({ field, fieldState }) => <>
+          <input
+            value={field.value}
+            onChange={(e) => {
+              console.log('FieldState - NumberField: ')
+              console.log(fieldState)
+              field.onChange(e)
+            }}
+          />
+          <span className="err">{!!fieldState.error && `error: ${fieldState.error.message}`}</span>
+        </>}
+      />
+      <input className="submitBtn" type="submit" />
+    </form>
+  );
+}
